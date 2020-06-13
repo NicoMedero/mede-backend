1ff979cf66201292a81fa3c89a80f6940a5e9271
Nicolás Medero <nicolasmederoc@gmail.com>
2020-06-13 17:29:30 -0300

    agrego parámetro en greetings y una sección index

--- src/main/java/com/personal/sample/backend/controllers/GreetingsController.java
+++ src/main/java/com/personal/sample/backend/controllers/GreetingsController.java
@@ -1,5 +1,17 @@
 package com.personal.sample.backend.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class GreetingsController {
 
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required = false, defaultValue = "")
+        String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting"; // The html to return
+    }
 }
